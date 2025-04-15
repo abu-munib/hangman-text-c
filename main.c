6ea35e3b73a5ad9f16a7229c2528cbf9e9c1ac2d
amengdv <aminkafri0082@gmail.com>
2025-04-15 13:12:42 +0800

    choose random words and print spaces

--- main.c
+++ main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 /*
  * Hangman guessing word games
  * Author: Abu munib
 */
 
+#define NUM_OF_WORDS 3
+#define TRIAL 9
+
+const char *words[NUM_OF_WORDS] = {
+	"Banana",
+	"Watermelon",
+	"Papaya"
+};
+
 int main() {
-	printf("Hello world\n");
+
+	printf("WELCOME TO HANGMANT\n");
+	printf("========================\n");
+
+	int word_index, word_len;
+	char user_input;
+
+	srand(time(NULL)); // seeds using current time 
+
+	// Choose random words when start
+	word_index = rand() % NUM_OF_WORDS;
+	const char *word = words[word_index];
+
+	// Print trials
+	printf("You have %d trials left\n", TRIAL);
+
+	// Determine word length and print spaces
+	word_len = strlen(word);
+	for (int i = 0; i < word_len; i++) {
+		printf("_");
+	}
+	printf("\n");
+	printf("========================\n");
+
+
+	
 	return 0;
 }
